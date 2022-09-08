3e8efaaffc04b7f37b529297a3c7e6324e0053a2
yyt <2298269347@qq.com>
2022-09-08 19:50:55 +0800

    自定义转换器

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from apps.users import views
-urlpatterns=[
+
+# 注册转换器
+from utils.converters import UsernameConverter
+from django.urls import register_converter
+
+register_converter(UsernameConverter, 'username')
+
+urlpatterns = [
     # 判断用户名是否重复
-    path('usernames/<username>/count/',views.UsernameCountView.as_view())
+    path('usernames/<username:username>/count/', views.UsernameCountView.as_view())
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
@@ -27,6 +29,8 @@ from apps.users.models import User
 class UsernameCountView(View):
     def get(self, request, username):
         # 1.  接收用户名，对这个用户名进行一下判断
+        # if not re.match('[a-zA-Z0-9_-]{5,20}',username):
+        #     return JsonResponse({'code':200,'errmsg':'用户名不满足需求'})
         # 2.  根据用户名查询数据库
         count = User.objects.filter(username=username).count()
         # 3.  返回响应
