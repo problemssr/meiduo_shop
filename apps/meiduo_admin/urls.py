d342bf57f03a52d9f0a9cb0ecbbc6c81df27be36
yyt <2298269347@qq.com>
2022-09-22 23:03:30 +0800

    日活用户统计

--- apps/meiduo_admin/urls.py
+++ apps/meiduo_admin/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from apps.meiduo_admin.views import MyTokenObtainPairView
+from apps.meiduo_admin.views import home
+from apps.meiduo_admin.views.token import MyTokenObtainPairView
 
 urlpatterns = [
     path('authorizations/', MyTokenObtainPairView.as_view()),
     # path('authorizations/', meiduo_token),
+    # 日活统计
+    path('statistical/day_active/', home.DailyActiveAPIView.as_view()),
 ]
--- apps/meiduo_admin/views/token.py
+++ apps/meiduo_admin/views/token.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from apps.meiduo_admin.serializers import MyTokenObtainPairSerializer
+from apps.meiduo_admin.serializers.token import MyTokenObtainPairSerializer
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
